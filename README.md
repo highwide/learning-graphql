5e9c2b189b45c5979da07788946804034a2e0a50
highwide <hochweit728@gmail.com>
2020-07-30 14:02:49 +0900

    Implement 5.1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-This repository is for "Learning Grahql" (「初めてのGraphQL」) by O'reily
+This repository is for "Learning Grahql" (「初めてのGraphQL」) by O'Reily
+
+## How to run
+
+```
+npm install apollo-server graphql nodemon
+```
--- index.js
+++ index.js
@@ -1,10 +1,50 @@
-const typeDefs= `
+const { ApolloServer } = require(`apollo-server`);
+
+const typeDefs = `
   type Query {
     totalPhotos: Int!
+    shuffledMembers: [String]!
   }
-`
+
+  type Mutation {
+    postPhoto(name: String! description: String): Boolean!
+  }
+`;
+
+let photos = [];
+
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
-  }
-}
+    totalPhotos: () => photos.length,
+    shuffledMembers: () =>
+      [
+        "kachick",
+        "pankona",
+        "mtsmfm",
+        "motorollerscalatron",
+        "nkmrh",
+        "mpls104",
+        "ujihisa",
+        "geckour",
+        "farmanlab",
+        "elliekwon",
+        "highwide",
+      ].sort(() => Math.random() - 0.5),
+  },
+
+  Mutation: {
+    postPhoto(_, args) {
+      photos.push(args);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen()
+  .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
