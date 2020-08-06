34a3bf4362d003a1e57678dee31b73e85f699cfa
pankona <yosuke.akatsuka@gmail.com>
2020-08-06 11:29:29 +0900

    add codes for 5.2.4.1

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ const typeDefs = `
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
+  }
+
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
   }
 
   input PostPhotoInput {
@@ -34,8 +42,35 @@ const typeDefs = `
   }
 `;
 
+let users = [
+  { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
+  { "githubLogin": "gPlake", "name": "Glen Plake" },
+  { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
+]
+
 let _id = 0
-let photos = [];
+
+let photos = [
+  {
+    "id": "1",
+    "name": "Dropping the Heart Chute",
+    "description": "The heart chute is one of my favorite chutes", "category": "ACTION",
+    "githubUser": "gPlake"
+  },
+  {
+    "id": "2",
+    "name": "Enjoying the sunshine",
+    "category": "SELFIE",
+    "githubUser": "sSchmidt"
+  },
+  {
+    "id": "3",
+    "name": "Gunbarrel 25",
+    "description": "25 laps on gunbarrel today", "category": "LANDSCAPE",
+    "githubUser": "sSchmidt"
+  }
+]
+
 
 const resolvers = {
   Query: {
@@ -70,7 +105,16 @@ const resolvers = {
   },
 
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser) 
+    }
+  },
+
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin)
+    } 
   }
 };
 
