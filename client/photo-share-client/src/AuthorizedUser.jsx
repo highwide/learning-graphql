e06f5f0bb6bbc744d7ff7257201ec04b13d29937
farmanlab <farmlanlabdev@gmail.com>
2020-10-02 14:03:08 +0900

    Implemented AuthorizedUser for 6.4.1

---
+++ client/photo-share-client/src/AuthorizedUser.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import {withReouter} from 'react-router-dom'
+
+class AuthorizedUser extends Component {
+    state = {signingIn: false}
+
+    componentDidMount() {
+        if (window.location.search.match(/code=/)) {
+            this.setState({signingIn: true})
+            const code = window.location.search.replace("?code=", "")
+            alert(code)
+            this.props.history.replace('/')
+        }
+    }
+
+    requestCode() {
+        const clientId = "FIX ME to YOUR_GITHUB_CLIENT_ID"
+        window.location = `https://github.com/login/oauth/authrize?client_id-${clientId}&scope=user`
+    }
+
+    render() {
+        return (
+            <button onClick={this.requestCode} disabled={this.state.signingIn}>
+                Sign In with GitHub
+            </button>
+        )
+    }
+
+}
+
+export default withRouter(AuthorizedUser)
