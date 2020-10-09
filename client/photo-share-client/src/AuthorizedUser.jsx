bd980259149ebadf19c53e5f41aea3c412827b93
Ellie Kwon <elliebkwon@gmail.com>
2020-10-09 14:06:23 +0900

    Progress 2020-10-09

--- client/photo-share-client/src/AuthorizedUser.jsx
+++ client/photo-share-client/src/AuthorizedUser.jsx
@@ -1,15 +1,34 @@
 import React, {Component} from 'react'
 import {withRouter} from 'react-router-dom'
+import {Mutation} from 'react-apollo'
+import {gql} from 'apollo-boost'
+import {ROOT_QUERY} from './App'
 
+const GITHUB_AUTH_MUTATION = gql`
+mutation githubAuth($code:String!) {
+    githubAuth(code:$code) { token }
+}
+`
 class AuthorizedUser extends Component {
     state = {signingIn: false}
+
+    authorizationComplete = (cache, {data}) => {
+        localStorage.setItem('token',data.githubAuth.token)
+        this.props.history.replace('/')
+        this.setState({signingIn:false})
+    }
 
     componentDidMount() {
         if (window.location.search.match(/code=/)) {
             this.setState({signingIn: true})
             const code = window.location.search.replace("?code=", "")
-            alert(code)
-            this.props.history.replace('/')
+
+            //--- Success
+            // alert(code)
+            // this.props.history.replace('/')
+
+            //--- Failed
+            this.githubAuthMutation({variables:{code}})
         }
     }
 
@@ -20,12 +39,20 @@ class AuthorizedUser extends Component {
 
     render() {
         return (
-            <button onClick={this.requestCode} disabled={this.state.signingIn}>
-                Sign In with GitHub
-            </button>
+            <Mutation mutation={GITHUB_AUTH_MUTATION}
+            update={this.autorizationComplete}
+            refetchQueries={[{query:ROOT_QUERY}]}>
+                {mutation => {
+                    this.githubAuthMutation = mutation
+                    return (
+                        <button onClick={this.requestCode} disabled={this.state.signingIn}>
+                        Sign In with GitHub
+                    </button>
+                    )
+                }}
+            </Mutation>
         )
     }
-
 }
 
 export default withRouter(AuthorizedUser)
